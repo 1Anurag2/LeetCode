5c402365dc58593d7a7fcdd4c6409ee731f3e7f7
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-01-18 21:42:22 +0530

    Solution

---
+++ Integer_to_Roman.py
@@ -0,0 +1,21 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        roman_numerals = [
+        ('M', 1000), ('CM', 900), ('D', 500), ('CD', 400),
+        ('C', 100), ('XC', 90), ('L', 50), ('XL', 40),
+        ('X', 10), ('IX', 9), ('V', 5), ('IV', 4), ('I', 1)
+        ]
+    
+        result = []
+        for symbol, value in roman_numerals:
+            while num >= value:
+                result.append(symbol)
+                num -= value
+    
+        return ''.join(result)
+
+sol = Solution()
+
+print(sol.intToRoman(3749))  
+print(sol.intToRoman(58))    
+print(sol.intToRoman(1994))  
