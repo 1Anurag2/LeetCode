de459acbe5e4b4b7ede0c3dcfbacc88010ac032d
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-01-29 23:00:05 +0530

    Solution

---
+++ Divide_Two_Integers.py
@@ -0,0 +1,9 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        if dividend == -2147483648 and divisor == -1:
+            return 2147483647
+        multipicant = dividend/divisor
+        return int(multipicant)
+        
+sol = Solution()
+print(int(sol.divide(7,-3)))
